ad1dbe2395254586401c046c95e48fc0a399d020
Yesudeep Mangalapilly <yesudeep@mia.local>
2010-02-17 17:36:46 +0530

    move lightbox to project page

--- media/script/project_information.js
+++ media/script/project_information.js
@@ -1,10 +1,11 @@
 jQuery(function(){
-  jQuery('.photos a').lightBox({
+  var lightboxOptions = {
     fixedNavigation: true,
     imageLoading: '/s/image/lightbox/lightbox-ico-loading.gif',
 		imageBtnPrev: '/s/image/lightbox/lightbox-btn-prev.gif',
 		imageBtnNext: '/s/image/lightbox/lightbox-btn-next.gif',
 		imageBtnClose: '/s/image/lightbox/lightbox-btn-close.gif',
 		imageBlank: '/s/image/lightbox/lightbox-blank.gif'
-  });
+  };
+  jQuery('.photos a').lightBox(lightboxOptions);
 });
