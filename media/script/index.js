7c0effd93a482b5db45aafad6189c260a2678c63
Doli Bhanushali <doli@waterloo.(none)>
2010-03-11 14:12:04 +0530

    latest changes

--- media/script/index.js
+++ media/script/index.js
@@ -1,3 +1,8 @@
 jQuery(function(){
-
+  //jQuery('#splash-photos >ul').innerfade({ 
+    //speed: 'slow', 
+    //timeout: 4000, 
+    //type: 'random', 
+    //containerheight: '220px' 
+  //});
 });
